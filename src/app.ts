b08ba73c0d91829911e802ee6fa6f165b28b81d2
Sebastián Suárez Valencia <suarez.valencia.sebastian@gmail.com>
2021-03-17 20:24:21 -0500

    Add travis deploy config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { mongoose } from './middlewares';
 import router from './modules';
 
@@ -7,6 +8,7 @@ const app = express();
 mongoose();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/health', (_, res) => {
   res.json({
